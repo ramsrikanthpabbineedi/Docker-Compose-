8b7c695e07ceb420ba4932f8023e082fb5a6b92b
ram srikanth pabbineedi <ramsrikanthpabbineedi@gmail.com>
2026-05-10 15:53:17 +0530

    ami fixed

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -3,6 +3,7 @@ variable "project_name"          {
     }
 variable "ami_id"                {
      type = string 
+     default = "ami-05d62b9bc5a6ca605"
      }
 variable "instance_type"         {
      type = string 
